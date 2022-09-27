b680a073bb97506eff4249392c1bec616239c9da
pedrohelias <pedrohelias95@hotmail.com>
2022-09-27 20:42:52 -0300

    hangman almost done. Troubles in comparation

---
+++ 16_hangman/script.rb
@@ -0,0 +1,101 @@
+class Jogador
+    attr_accessor :name
+
+    def initialize
+    end
+
+    public def inserirNome
+        puts "insira o nome do Jogador"
+        @nome = gets.chomp
+    end
+
+
+end
+
+
+class Hangman
+    attr_accessor :classeJogador, :arquivo, :armazenador, :palavraTurno
+
+    def initialize(classeJogador, arquivo)
+        @classeJogador = classeJogador
+        #@dicionario = dicionario
+        @arquivo = arquivo
+
+    end
+
+    def preencheDic
+    
+        @armazenador = []
+        n = 0
+        File.open(@arquivo, "r") do |ponteiroLeitura|
+            while newLine = ponteiroLeitura.gets #realizando a leitura em cada linha do arquivo de texto
+                #puts newLine
+                @armazenador[n] = newLine.delete("\n") #vai guardar os dados do dicionario e deletar as quebras de barra
+                n = n + 1 #adicionar o contador e andar dentro da variável que vai armazenar o dicionario
+            end
+    
+        end
+
+        return @armazenador
+    end
+
+    def selecionaPalavra
+        randomizador = rand(0..4)
+        puts "o randomizador é #{randomizador}"
+        return @palavraTurno = @armazenador[randomizador]
+    end
+
+
+    public def jogar
+        letrasUtilizadas = []
+        rodada = 0
+        count = 0
+        palavra = selecionaPalavra() #vai selecionar a palavra do turno do jogo
+
+        palavraSplit = palavra.split("") #para simplificar na hora de adicionar as letras encontradas pelo jogador
+
+        palavraJogador = Array.new(palavraSplit.length) #criando um array que vai copiar o tamanho do array a ser encontrado. Composto por Nills...
+
+        while palavraSplit != palavraJogador
+            
+            puts "Selecione uma letra"
+            puts "As letras já utilizadas foram: #{letrasUtilizadas}" #vai controlar as letras que ja foram utilizadas para o jogaodor não repetir
+            letrasUtilizadas = gets.chomp 
+
+            letrasUtilizadas[rodada].each_with_index do |ponteiroLetras,index| #vai pegar a letrar e comparar com a palavra selecionada a ser descoberta.
+                
+                loop do
+                    if ponteiroLetras == palavraSplit[count]
+                        palavraJogador[index] = palavraSplit[index]
+                        count = count + 1
+                    else
+                        count = count + 1
+                    end
+
+                    break if count == palavraSplit.length
+                end
+
+                puts "a palavra está desse jeito: #{palavraJogador}"
+            
+            end
+
+
+
+            rodada = rodada + 1 #vai rotacionar o contador das letras utilizadas 
+        
+        
+        end
+
+
+    end
+
+
+end
+
+jogador1 = Jogador.new()
+jogador1.inserirNome
+
+
+hangman1 = Hangman.new(jogador1, "respostas.txt")
+hangman1.preencheDic
+hangman1.jogar
